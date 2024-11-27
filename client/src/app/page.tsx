a7239e3d7d55b0ca72634ef5dd4559dc63d8139b
Cleber Lucas Farias <clfm@cin.ufpe.br>
2024-11-27 20:19:46 -0300

    Remove topbar from page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,7 +6,6 @@ import { LogoCITi } from "../assets";
 export default function Home() {
   return (
     <div className="flex flex-1 flex-col h-full justify-around items-center bg-black">
-      <Topbar/>
       <div>
         <Image src={LogoCITi} alt="Logo citi" />
       </div>
