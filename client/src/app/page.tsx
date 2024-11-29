7d05049115bef522396bc1e64e04e3f796ada0a6
luizacavalcantee <cavalcanteluiza13@gmail.com>
2024-11-29 17:07:57 -0300

    component correction

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,20 +1,7 @@
-import Image from "next/image";
-
-import { LogoCITi } from "../assets";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col h-full justify-center items-center bg-white">
-      <div>
-        <Image src={LogoCITi} alt="Logo citi" />
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="text-black text-4xl font-bold">NextJS Boilerplate</h1>
-        <p className="text-black text-xl">
-          Made with <strong>&lt; &#x0002F; &gt;</strong> and{" "}
-          <strong>&hearts;</strong> by CITi
-        </p>
-      </div>
+    <div className="flex flex-col h-full w-full items-center bg-background">
     </div>
   );
 }
--- client/src/components/sidebar/index.tsx
+++ client/src/components/sidebar/index.tsx
@@ -11,9 +11,9 @@ export default function Sidebar() {
   const router = useRouter();
   
   return (
-    <div className="sidebar flex flex-col h-full items-center justify-between bg-backgroundSidebar w-64">
+    <div className="flex flex-col h-full items-center justify-between bg-backgroundSidebar w-64 p-6">
 
-      <div className="sidebar_UpSide flex flex-col items-center gap-4 py-7">
+      <div className="flex flex-col items-center gap-4 py-7">
         <Image src={logoCITiWhite} alt="Logo" className="itens-center w-30 h-auto mb-4"/>
 
         <Button onClick={() => router.push("/dashboard")} className="flex items-center justify-start w-56 h-11 px-4 bg-white bg-opacity-40 [&_svg]:size-8 hover:bg-black/20 hover:shadow-xl">
@@ -27,7 +27,7 @@ export default function Sidebar() {
         </Button>
       </div>
 
-      <div className="sidebar_footer flex flex-row items-center gap-2 p-4">
+      <div className="flex flex-row items-center gap-2">
         <p className="text-white text-sm font-barlow">
           Made with <strong>&lt; &#x0002F; &gt;</strong> and{" "}<strong>&hearts;</strong> by{" "}
         </p>
