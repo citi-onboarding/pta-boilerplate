5f51cb113629c0cc4d6d78ea3f7f0c722d251a03
dudarmouras <dudarmouras@gmail.com>
2024-11-28 23:03:57 -0300

    create button

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,6 +2,20 @@ import Image from "next/image";
 
 import { LogoCITi } from "../assets";
 
+import CreateButton from "@/components/create-button";
+
+export default function Home() {
+  return (
+    <div className="ml-8">
+      <CreateButton>
+      
+       
+      </CreateButton>
+    </div>
+  );
+}
+
+
 export default function Home() {
   return (
     <div className="flex flex-1 flex-col h-full justify-around items-center bg-black">
