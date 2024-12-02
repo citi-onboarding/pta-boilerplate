c0af2550e0853c3cd5615c56e65cc90af6119220
luizacavalcantee <cavalcanteluiza13@gmail.com>
2024-12-02 12:00:38 -0300

    Scroll

--- client/src/app/ExploreMatches/index.tsx
+++ client/src/app/ExploreMatches/index.tsx
@@ -1,16 +1,18 @@
 import { CreateButton } from "@/components/CreateButton";
 import MatchCard from "@/components/MatchCard";
-import Topbar from "@/components/topbar";
+import Topbar from "@/components/Topbar";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 export default function ExploreMatches() {
     return (
       <div>
         <Topbar backArrow={false}/>
-        <div className="flex flex-col mx-16 my-12">
+        <div className="flex flex-col mx-16 my-12 h-full">
             <h1 className="mb-16 text-2xl text-black font-barlow font-normal">
             Próximas partidas
             </h1>
-            <div className="gap-8 grid grid-cols-4 mb-8">
+            <ScrollArea className="w-full rounded-md border p-2 mb-8">
+              <div className="flex flex-wrap gap-8 justify-around">
                 <MatchCard status={"closed"}/>
                 <MatchCard status={"full"}/>
                 <MatchCard status={"joinable"}/>
@@ -35,7 +37,9 @@ export default function ExploreMatches() {
                 <MatchCard status={"full"}/>
                 <MatchCard status={"joinable"}/>
                 <MatchCard status={"playing"}/>
-            </div>
+              </div>
+              <ScrollBar orientation="horizontal" />
+            </ScrollArea>
             <CreateButton/>
         </div>
       </div>
